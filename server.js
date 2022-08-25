7cee8cfd91b839018053033605713fbf1c8c5ecb
hannahbethke <hannahnichole@gmail.com>
2022-08-25 11:41:41 -0600

    MongoDB and Express are connected

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const { DATABASE_URL } = process.env;
+const PORT = process.env.PORT || 4000;
+const app = express(); 
+
+mongoose.connect(DATABASE_URL);
+mongoose.connection
+    .on("open", () => console.log("Connected to MongoDB"))
+    .on("close", () => console.log("Disconnected from MongoDB"))
+    .on("error", (error) => console.log('MongoDB Error:' + error.message));
+
+app.use('/public', express.static('public'));
+app.use(methodOverride('_method'));
+app.use(morgan("dev"));
+// TODO:
+// app.use(express.urlencoded()) -> creates req.body: use if you are doing server side rendering
+// app.use(express.json()) -> creates req.body: use if you are using AJAX, api requests
+// app.use('/nameOfController', nameOfController);
+// app.use(cors()); need cors or no if not using an API? 
+// app.use('/api/portfolio', nameofRouter);
+
+app.get('/', (req, res) => {
+    res.send('Portfolio API');
+});
+
+// TODO: need?
+app.get('/*', (req, res) => {
+    res.status(404).json({ message: 'not found' })
+});
+
+// Tell the App to Listen
+app.listen(PORT, () => {
+    console.log(`Express is listening on port:${PORT}`);
+});
