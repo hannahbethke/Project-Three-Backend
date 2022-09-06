71bf5da00e54fbaa478b163a4774df93d92da4e9
hannahbethke <hannahnichole@gmail.com>
2022-09-06 02:15:29 -0600

    changes model from song to artwork

--- models/song.js
+++ models/song.js
@@ -4,12 +4,9 @@ const Schema = mongoose.Schema;
 const songSchema = new Schema({
     title: String,
     artist: String,
-    albumTitle: String,
-    image: { 
-        type: String,
-        default: "https://cdn.pixabay.com/photo/2020/05/11/17/42/vinyl-5159342_960_720.jpg"
-    },
-    released: String
+    photoCollection: String,
+    image: String,
+    dateAdded: String
 }, { timestamps: true});
 
 module.exports = mongoose.model('Song', songSchema);
